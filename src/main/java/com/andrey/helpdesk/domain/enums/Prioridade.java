c5063c7a8fbdfbc19fcef2a02906c9d7a551b77b
Andrey <andreyc3@gmail.com>
2023-08-12 15:14:05 -0300

    Criação de Tipos Enumerados: Prioridade, Perfil e Status

---
+++ src/main/java/com/andrey/helpdesk/domain/enums/Prioridade.java
@@ -0,0 +1,28 @@
+package com.andrey.helpdesk.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Prioridade {
+    BAIXA(0, "BAIXA"), MEDIA(1, "MÉDIA"), ALTA(2, "ALTA");
+
+    private Integer codigo;
+    private String descricao;
+
+    public static Prioridade toEnum(Integer codigo){
+        if(codigo == null){
+            return null;
+        }
+
+        for(Prioridade x : Prioridade.values()){
+            if(codigo.equals(x.getCodigo())){
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Prioridade Inválida");
+    }
+
+}
